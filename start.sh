08d1318be97fd457f98ab081fdd6f9cedfa95dee
Milan Kodali <milankodali@gmail.com>
2025-10-24 12:57:11 -0400

    WIP CLIP: TextDecoder & TextTokenizer

---
+++ start.sh
@@ -0,0 +1,10 @@
+# check if uv is installed; install it if not
+command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+# check if .venv dir exists; create a venv if not
+[ -d ".venv" ] || uv venv
+# install the repo dependencies
+uv sync
+# activate venv so that `python` uses the project's venv instead of system python
+source .venv/bin/activate
+# start jupyter (uncomment if not using vscode)
+# jupyter notebook
